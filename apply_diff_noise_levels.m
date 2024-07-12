7184d6c9c44247c807f52da001aba6ab1e799239
Tahereh Rashnavadi <54557830+rashnavadi@users.noreply.github.com>
2024-07-12 13:55:22 -0600

    Update apply_diff_noise_levels.m
    a for loop added to go through different noise levels to be added to the timeseries of x, y and z.

--- apply_diff_noise_levels.m
+++ apply_diff_noise_levels.m
@@ -1,7 +1,7 @@
 %% July 2024, By Tahereh Rashnavadi
 clear;
 close all;
-Basefile= '/Users/trashnavadi/Documents/Data_Analysis/2022/analyses/kmeans_investigation/2024/July/testing';
+Basefile= '/Users/trashnavadi/Documents/Data_Analysis/2022/analyses/kmeans_investigation/2024/July';
 
 % Initialize variables
 % Number of iterations
@@ -13,10 +13,16 @@ nStates = 2;
 TR = 2; % in sec
 time = linspace(0, (nTimePts-1) * TR, nTimePts);
 
-transitionPoint = 75;  % Transition between states happens at this point
+% transitionPoint = [15, 45, 75, 120] % in TR, which are [30, 90, 150, 240]
+% in seconds
+transitionPoint = 75;  % Transition between states happens at this point in TR
 k = nStates;
-% WSize = [38, 50, 76, 100];
-WSize = 50; % in timepoints (TRs), 300 sec
+% WSize = [30, 40, 50]; % in timepoints (TR = 2sec)
+WSize = 50; % in timepoints (TR = 2sec)
+
+% Define noise levels to test
+noise_levels = [10, 100, 1000, 5000, 10000]; % Adjust these levels as needed
+num_noise_levels = length(noise_levels);
 
 % Load the original noise-free time series from a file (assuming it's in an Excel file)
 load('orig_timeseries.mat')
@@ -45,121 +51,124 @@ all_noisy_z = zeros(nTimePts, numIterations);
 desired_snr = 50;
 
 
-% Run the function for 1000 iterations
-for iter = 1:numIterations
-
-    % Add 10000 to mean of the timeseries, and Calculate the scaling factor
-    noise_std_x = (mean(x)+10000)/desired_snr;
-    noise_std_y = (mean(y)+10000)/desired_snr;
-    noise_std_z = (mean(z)+10000)/desired_snr;
+for n = 1:num_noise_levels
+    noise_level = noise_levels(n);
+    % Run the function for 1000 iterations
     
-    % generate pink noise independently to each timeseries, 
-    % Generate pink noise for x, y, and z
-    pink_noise_x = pinknoise(nTimePts, 1);
-    pink_noise_y = pinknoise(nTimePts, 1);
-    pink_noise_z = pinknoise(nTimePts, 1);
+    for iter = 1:numIterations
+        % Add 10000 to mean of the timeseries, and Calculate the scaling factor
+        noise_std_x = (mean(x)+10000)/desired_snr;
+        noise_std_y = (mean(y)+10000)/desired_snr;
+        noise_std_z = (mean(z)+10000)/desired_snr;
 
-    % Generate Gaussian noise with the same standard deviation as the pink noise
-    gaussian_noise_x = randn(nTimePts, 1);
-    gaussian_noise_y = randn(nTimePts, 1);
-    gaussian_noise_z = randn(nTimePts, 1);
+        % generate pink noise independently to each timeseries,
+        % Generate pink noise for x, y, and z
+        pink_noise_x = pinknoise(nTimePts, 1);
+        pink_noise_y = pinknoise(nTimePts, 1);
+        pink_noise_z = pinknoise(nTimePts, 1);
 
-    % Combine pink noise and Gaussian noise
-    combined_noise_x = pink_noise_x + gaussian_noise_x;
-    combined_noise_y = pink_noise_y + gaussian_noise_y;
-    combined_noise_z = pink_noise_z + gaussian_noise_z;
+        % Generate Gaussian noise with the same standard deviation as the pink noise
+        gaussian_noise_x = noise_level .* randn(nTimePts, 1);
+        gaussian_noise_y = noise_level .* randn(nTimePts, 1);
+        gaussian_noise_z = noise_level .* randn(nTimePts, 1);
 
-    % Calculate the combined noise standard deviation
-    current_std_combined_noise_x = std(combined_noise_x);
-    current_std_combined_noise_y = std(combined_noise_y);
-    current_std_combined_noise_z = std(combined_noise_z);
+        % Combine pink noise and Gaussian noise
+        combined_noise_x = pink_noise_x + gaussian_noise_x;
+        combined_noise_y = pink_noise_y + gaussian_noise_y;
+        combined_noise_z = pink_noise_z + gaussian_noise_z;
 
-    % Calculate the scaling factor to achieve the desired total standard deviation
-    scaling_factor_x = noise_std_x / current_std_combined_noise_x;
-    scaling_factor_y = noise_std_y / current_std_combined_noise_y;
-    scaling_factor_z = noise_std_z / current_std_combined_noise_z;
+        % Calculate the combined noise standard deviation
+        current_std_combined_noise_x = std(combined_noise_x);
+        current_std_combined_noise_y = std(combined_noise_y);
+        current_std_combined_noise_z = std(combined_noise_z);
 
-    % Scale the combined noise
-    scaled_combined_noise_x = combined_noise_x * scaling_factor_x;
-    scaled_combined_noise_y = combined_noise_y * scaling_factor_y;
-    scaled_combined_noise_z = combined_noise_z * scaling_factor_z;
+        % Calculate the scaling factor to achieve the desired total standard deviation
+        scaling_factor_x = noise_std_x / current_std_combined_noise_x;
+        scaling_factor_y = noise_std_y / current_std_combined_noise_y;
+        scaling_factor_z = noise_std_z / current_std_combined_noise_z;
 
-    % Add the scaled combined noise to the original signals
-    noisy_x = 1500 * x  + scaled_combined_noise_x;
-    noisy_y = 1500 * y  + scaled_combined_noise_y;
-    noisy_z = 1500 * z  + scaled_combined_noise_z;
-    noisy_x = zscore(noisy_x);
-    noisy_y = zscore(noisy_y);
-    noisy_z = zscore(noisy_z);
-   
-    % Store the noisy signals
-    all_noisy_x(:, iter) = noisy_x;
-    all_noisy_y(:, iter) = noisy_y;
-    all_noisy_z(:, iter) = noisy_z;
+        % Scale the combined noise
+        scaled_combined_noise_x = combined_noise_x * scaling_factor_x;
+        scaled_combined_noise_y = combined_noise_y * scaling_factor_y;
+        scaled_combined_noise_z = combined_noise_z * scaling_factor_z;
 
-    % Verify the standard deviation of the noise
-    final_noise_x = noisy_x - x;
-    final_noise_y = noisy_y - y;
-    final_noise_z = noisy_z - z;
-    final_std_noise_x = std(final_noise_x);
-    final_std_noise_y = std(final_noise_y);
-    final_std_noise_z = std(final_noise_z);
+        % Add the scaled combined noise to the original signals
+        noisy_x = 1500 * x  + scaled_combined_noise_x;
+        noisy_y = 1500 * y  + scaled_combined_noise_y;
+        noisy_z = 1500 * z  + scaled_combined_noise_z;
+        noisy_x = zscore(noisy_x);
+        noisy_y = zscore(noisy_y);
+        noisy_z = zscore(noisy_z);
 
-    % Display the result
-    disp(['Final standard deviation of the noise for x: ', num2str(final_std_noise_x)]);
-    disp(['Final standard deviation of the noise for y: ', num2str(final_std_noise_y)]);
-    disp(['Final standard deviation of the noise for z: ', num2str(final_std_noise_z)]);
+        % Store the noisy signals
+        all_noisy_x(:, iter) = noisy_x;
+        all_noisy_y(:, iter) = noisy_y;
+        all_noisy_z(:, iter) = noisy_z;
 
-    % Prepare data for HOCo and SWC
-    TSs = cell(2, 3);
-    TSs{1, 1} = 'timeseries_1';
-    TSs{2, 1} = all_noisy_x(:, iter);
-    TSs{1, 2} = 'timeseries_2';
-    TSs{2, 2} = all_noisy_y(:, iter);
-    TSs{1, 3} = 'timeseries_3';
-    TSs{2, 3} = all_noisy_z(:, iter);
-    save(fullfile(Basefile, 'sim_timeseries_1.mat'), 'TSs')
+        % Verify the standard deviation of the noise
+        final_noise_x = noisy_x - x;
+        final_noise_y = noisy_y - y;
+        final_noise_z = noisy_z - z;
+        final_std_noise_x = std(final_noise_x);
+        final_std_noise_y = std(final_noise_y);
+        final_std_noise_z = std(final_noise_z);
 
-    % HOCo and SWC processing
-    load(fullfile(Basefile, 'sim_timeseries_1.mat'))
-    SaveSuffix = 'Pearsons'; SurrOption = 0;
-    Time_series_file = fullfile(Basefile, 'sim_timeseries_1.mat');
-    hoco_FunConn(Time_series_file, WSize, SurrOption, SaveSuffix)
-    load(fullfile(Basefile, 'mtx_SldWFCPearsons.mat'))
-    FC_21 = SldWFCArray{3, 2};
-    FC_31 = SldWFCArray{4, 2};
-    FC_32 = SldWFCArray{4, 3};
+        % Display the result
+        disp(['Final standard deviation of the noise for x: ', num2str(final_std_noise_x)]);
+        disp(['Final standard deviation of the noise for y: ', num2str(final_std_noise_y)]);
+        disp(['Final standard deviation of the noise for z: ', num2str(final_std_noise_z)]);
 
-    % SWC Analysis
-    kmeans_data_SWC = [FC_21, FC_31, FC_32];
-    [StateIdx_SWC, Centroid_SWC, SumDistPartial_SWC] = kmeans(kmeans_data_SWC, k, 'Distance', 'cityblock', 'MaxIter', 100, 'Replicates', 100);
-    StateFlip_SWC{iter, 1} = find(logical(diff(StateIdx_SWC)));
+        % Prepare data for HOCo and SWC
+        TSs = cell(2, 3);
+        TSs{1, 1} = 'timeseries_1';
+        TSs{2, 1} = all_noisy_x(:, iter);
+        TSs{1, 2} = 'timeseries_2';
+        TSs{2, 2} = all_noisy_y(:, iter);
+        TSs{1, 3} = 'timeseries_3';
+        TSs{2, 3} = all_noisy_z(:, iter);
+        save(fullfile(Basefile, 'sim_timeseries_1.mat'), 'TSs')
 
-    % HOCo Analysis
-    X_full = TSs{2, 1};
-    Obs_full = TSs{2, 2};
-    T_full = TSs{2, 3};
-    param11 = 1; param12 = 1; param21 = 1;
+        % HOCo and SWC processing
+        load(fullfile(Basefile, 'sim_timeseries_1.mat'))
+        SaveSuffix = 'Pearsons'; SurrOption = 0;
+        Time_series_file = fullfile(Basefile, 'sim_timeseries_1.mat');
+        hoco_FunConn(Time_series_file, WSize, SurrOption, SaveSuffix)
+        load(fullfile(Basefile, 'mtx_SldWFCPearsons.mat'))
+        FC_21 = SldWFCArray{3, 2};
+        FC_31 = SldWFCArray{4, 2};
+        FC_32 = SldWFCArray{4, 3};
 
-    [static12, dynamic12] = hoco_fbglmfit(X_full, Obs_full, WSize, param11, param12, param21, TR);
-    [static13, dynamic13] = hoco_fbglmfit(X_full, T_full, WSize, param11, param12, param21, TR);
-    [static23, dynamic23] = hoco_fbglmfit(Obs_full, T_full, WSize, param11, param12, param21, TR);
+        % SWC Analysis
+        kmeans_data_SWC = [FC_21, FC_31, FC_32];
+        [StateIdx_SWC, Centroid_SWC, SumDistPartial_SWC] = kmeans(kmeans_data_SWC, k, 'Distance', 'cityblock', 'MaxIter', 100, 'Replicates', 100);
+        StateFlip_SWC{iter, 1} = find(logical(diff(StateIdx_SWC)));
 
-    % ========================================
-    %     % before running kmeans, trim the HOCo results, remove the FC matrices obtained from
-    % shrinking/growing windows , because the truncated HOCo results were
-    % better than the HOCo itself
-%         dynamic12.bb  = dynamic12.bb(WSize/2 : size(X_full) - WSize/2);
-%         dynamic13.bb  = dynamic13.bb(WSize/2 : size(X_full) - WSize/2);
-%         dynamic23.bb  = dynamic23.bb(WSize/2 : size(X_full) - WSize/2);
+        % HOCo Analysis
+        X_full = TSs{2, 1};
+        Obs_full = TSs{2, 2};
+        T_full = TSs{2, 3};
+        param11 = 1; param12 = 1; param21 = 1;
 
-    kmeans_data_HOCo = [nonzeros(dynamic12.bb), nonzeros(dynamic13.bb), nonzeros(dynamic23.bb)];
-    [StateIdx_HOCo, Centroid_HOCo, SumDistPartial_HOCo] = kmeans(kmeans_data_HOCo, k, 'Distance', 'cityblock', 'MaxIter', 100, 'Replicates', 100);
-    StateFlip_HOCo{iter, 1} = find(logical(diff(StateIdx_HOCo)));
+        [static12, dynamic12] = hoco_fbglmfit(X_full, Obs_full, WSize, param11, param12, param21, TR);
+        [static13, dynamic13] = hoco_fbglmfit(X_full, T_full, WSize, param11, param12, param21, TR);
+        [static23, dynamic23] = hoco_fbglmfit(Obs_full, T_full, WSize, param11, param12, param21, TR);
+
+        % ========================================
+        %     % before running kmeans, trim the HOCo results, remove the FC matrices obtained from
+        % shrinking/growing windows , because the truncated HOCo results were
+        % better than the HOCo itself
+        %         dynamic12.bb  = dynamic12.bb(WSize/2 : size(X_full) - WSize/2);
+        %         dynamic13.bb  = dynamic13.bb(WSize/2 : size(X_full) - WSize/2);
+        %         dynamic23.bb  = dynamic23.bb(WSize/2 : size(X_full) - WSize/2);
+
+        kmeans_data_HOCo = [nonzeros(dynamic12.bb), nonzeros(dynamic13.bb), nonzeros(dynamic23.bb)];
+        [StateIdx_HOCo, Centroid_HOCo, SumDistPartial_HOCo] = kmeans(kmeans_data_HOCo, k, 'Distance', 'cityblock', 'MaxIter', 100, 'Replicates', 100);
+        StateFlip_HOCo{iter, 1} = find(logical(diff(StateIdx_HOCo)));
+    end
+    % Save the results for each noise level
+    save(fullfile(Basefile, ['timeseries_with_noise_level_' num2str(noise_level) '.mat']), 'all_noisy_x', 'all_noisy_y', 'all_noisy_z', 'StateFlip_HOCo', 'StateFlip_SWC');
 end
 
-% Save the results to a .mat file
-save('timeseries_with_noise.mat', 'all_noisy_x', 'all_noisy_y', 'all_noisy_z', 'StateFlip_HOCo', 'StateFlip_SWC');
 
 % Plot the original signals and the noisy signals
 figure;
