1c0a7f8950acb13f8079588c59b6e31467a76b49
Tahereh Rashnavadi <54557830+rashnavadi@users.noreply.github.com>
2024-07-29 17:22:22 -0600

    Update find_inflection_points.m
    if derivate is zeros added

--- find_inflection_points.m
+++ find_inflection_points.m
@@ -1,5 +1,6 @@
 function [forward_inflection, backward_inflection, correlation_forward, correlation_backward] = find_inflection_points(X, transition_point)
-    % X: data points (n x d matrix) representing time series of correlations
+    % X: data points (n x d matrix) representing time series of
+    % correlations, X=[x,y,z] size: 150x3
     % transition_point: the transition point identified by K-means clustering
 
     % Step 1: Compute the derivative of the correlation
@@ -8,7 +9,7 @@ function [forward_inflection, backward_inflection, correlation_forward, correlat
     % Step 2: Find the forward inflection point
     forward_inflection = transition_point;
     for j = transition_point:size(delta_X, 1)
-        if j < size(delta_X, 1) && delta_X(j) * delta_X(j + 1) < 0 % Change in polarity
+        if j < size(delta_X, 1) && (delta_X(j) * delta_X(j + 1) < 0 || delta_X(j) == 0 || delta_X(j + 1) == 0) % Change in polarity or zero derivative
             forward_inflection = j + 1;
             break;
         end
@@ -17,7 +18,7 @@ function [forward_inflection, backward_inflection, correlation_forward, correlat
     % Step 3: Find the backward inflection point
     backward_inflection = transition_point;
     for j = transition_point:-1:2
-        if delta_X(j - 1) * delta_X(j) < 0 % Change in polarity
+        if (delta_X(j - 1) * delta_X(j) < 0 || delta_X(j - 1) == 0 || delta_X(j) == 0) % Change in polarity or zero derivative
             backward_inflection = j;
             break;
         end
